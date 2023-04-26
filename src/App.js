436c5c62d7f4ea8b0c08a02ba156fadbde8d8733
Arvind <thakurarvindkr10@gmail.com>
2023-04-26 15:00:18 +0530

    added NewExpenses Dir Now i want to add the form to add task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Expenses from './components/Expenses/Expenses'
-import Card from './components/UI/Card'
+import NewExpenses from './components/NewExpenses/NewExpenses'
 
 const App = () => {
     let expense = [
@@ -9,9 +9,14 @@ const App = () => {
         { id: 'e3', title: 'House Rent', amount: 750, date: new Date(2023, 5, 26) },
         { id: 'e4', title: 'Food', amount: 650, date: new Date(2023, 5, 29) },
     ]
+
+    const addExpenseHandler = (expense) => {
+        console.log(expense)
+    }
+
     return (
         <>
-            <h2>Let's get started</h2>
+            <NewExpenses />
             <Expenses item={expense} />
         </>
     )
---
+++ src/components/NewExpenses/NewExpenses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewExpenses = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NewExpenses;
