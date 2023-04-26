51e713d21713087e776f1e02d765b02c482d4e7d
Arvind <thakurarvindkr10@gmail.com>
2023-04-26 13:29:59 +0530

    removed button and input field also

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,29 +4,13 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = ({ exepenseDate, expenseTitle, expenseAmount }) => {
-    const [newTitle, setNewTitle] = useState('');
-    const [title, setTitle] = useState(expenseTitle);
-
-    const clickHandler = () => {
-        if (newTitle) {
-            setTitle(newTitle)
-            setNewTitle('')
-        } else {
-            alert('enter the fields')
-        }
-    }
-    const changeHandler = (e) => {
-        setNewTitle(e.target.value)
-    }
     return (
         <Card className='expense-item'>
             <ExpenseDate exepenseDate={exepenseDate} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{expenseTitle}</h2>
                 <div className='expense-item__price'>{expenseAmount} $</div>
             </div>
-            <input required onChange={changeHandler} value={newTitle} type="text" style={{ marginLeft: '10px', borderRadius: '12px', padding: '10px' }} />
-            <button onClick={clickHandler} style={{ marginLeft: '10px', borderRadius: '12px', padding: '10px' }}>Change Title</button>
         </Card>
     )
 }
