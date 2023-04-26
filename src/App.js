92ec88db861061566ada447aa0c026e94a25a79b
Arvind <thakurarvindkr10@gmail.com>
2023-04-26 15:43:07 +0530

    added fuctionality in the form

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
 
     return (
         <>
-            <NewExpenses/>
+            <NewExpenses />
             <Expenses item={expense} />
         </>
     )
--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+    }
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        
+        console.log(expenseData)
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
+    }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense_controls'>
                 <div className='new-expense_control'>
-                    <label htmlFor="">Title</label>
-                    <input type="text" />
+                    <label>Title</label>
+                    <input type="text" onChange={titleChangeHandler} value={enteredTitle} required />
                 </div>
                 <div className='new-expense_control'>
-                    <label htmlFor="">Amount</label>
-                    <input type="number" min={0.01} step={0.01} />
+                    <label>Amount</label>
+                    <input type="number" min={0.01} step={0.01} onChange={amountChangeHandler} value={enteredAmount} required />
                 </div>
                 <div className='new-expense_control'>
-                    <label htmlFor="">Date</label>
-                    <input type="date" />
+                    <label>Date</label>
+                    <input type="date" onChange={dateChangeHandler} value={enteredDate} required />
                 </div>
             </div>
             <div className='new-expense_actions'>
