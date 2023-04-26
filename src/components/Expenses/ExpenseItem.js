ed1053cf2a06f750a196030f63970330823fa939
Arvind <thakurarvindkr10@gmail.com>
2023-04-26 10:52:32 +0530

    added button to change title with help of useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = ({ exepenseDate, expenseTitle, expenseAmount }) => {
+    const [title, setTitle] = useState(expenseTitle);
+    const clickHandler = () => {
+        setTitle('New Title....')
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate exepenseDate={exepenseDate} />
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>{expenseAmount} $</div>
             </div>
+            <button onClick={clickHandler} style={{ marginLeft: '10px', borderRadius: '12px', padding: '10px' }}>Change Title</button>
         </Card>
     )
 }
